3842b6e5b9fec7efd2cbe87ff19817f0d6ab6a4f
isoulemane <snasnad@gmail.com>
2025-11-01 16:52:54 -0400

    Enhance outputs with private IPs and AZs
    Added outputs for private IPs and availability zones of instances.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-
-resource "aws_s3_bucket" "soules3"{
-}    
-
-output "bucket_name"{
-  value = "soules3"
-}
-
 # Output the ID of the first instance
 output "first_instance_id" {
   value = aws_instance.abdul[0].id
@@ -16,6 +8,16 @@ output "first_instance_public_ip" {
   value = aws_instance.abdul[0].public_ip
 }
 
+# Output the private IP of the first instance
+output "first_instance_private_ip" {
+  value = aws_instance.abdul[0].private_ip
+}
+
+# Output the availability zone of the first instance
+output "first_instance_az" {
+  value = aws_instance.abdul[0].availability_zone
+}
+
 # Output the IDs of all instances
 output "all_instance_ids" {
   value = aws_instance.abdul[*].id
@@ -24,4 +26,14 @@ output "all_instance_ids" {
 # Output the public IPs of all instances
 output "all_instance_public_ips" {
   value = aws_instance.abdul[*].public_ip
+}
+
+# Output the private IPs of all instances
+output "all_instance_private_ips" {
+  value = aws_instance.abdul[*].private_ip
+}
+
+# Output the availability zones of all instances
+output "all_instance_azs" {
+  value = aws_instance.abdul[*].availability_zone
 }
